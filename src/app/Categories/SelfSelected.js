10f7fdd11d0db4556b636b136255eafc109be02e
Zachariah <riahdrown1@gmail.com>
2024-11-10 01:52:48 -0500

    added list

---
+++ src/app/Categories/SelfSelected.js
@@ -0,0 +1,99 @@
+/* news including: 
+-physics
+        twitter account
+        somephyscs journal 
+-math 
+        somemathjournal 
+       twitter account 
+-economics -only update if there is a change in the 10yr treasury, interst rate
+      twitter account, 
+      nature 
+-materail science 
+      nature 
+     twitter account
+-robotics
+    robotics site search
+    twitter news 
+
+-hackernews 
+
+software and company news 
+    twitter 
+    techcrunch 
+
+ai/ tech innovation news 
+    twitter 
+
+    medical innovation and device innovation 
+
+    nueroscience / cellular biology/ genetics (biology)
+           twitter accounts 
+
+general twitter account - musk, rogan, peterson, lex fridman, ?? 
+     - 
+
+
+     categories : technology and software 
+     - ai news 
+     -medical/ healthtech news 
+     -hardware
+     -software and platforms 
+     -twitter
+     -robotics
+     -energy systems 
+
+     science and math 
+     - material scince 
+     -phsycs
+     -math/ number theory 
+     - hacker news 
+     -twitter
+
+     biology
+     - cellular biology
+     -genetics 
+     -nueroscience 
+
+
+
+     learn 
+
+     math 
+     -number thoery 
+     -calculus 
+     -trigonometry 
+     -set theory 
+     -group theory 
+
+     science 
+     -physics
+     -comp science 
+     -logic 
+     -mechanical engineering 
+     -material science 
+     - chemistry 
+     -economics 
+
+     history and historic battles 
+
+
+
+     biology 
+     -nuero 
+     -cellular pathwyas and cellular anamtomy 
+     -genetics 
+
+
+    
+
+business 
+     business leaders key characterists and examples 
+     best products or softwares ever and why 
+
+     lines from best songs 
+     story telling best lines or scenes
+
+     1 maybe edgy maybe not great joke 
+
+ */
+
